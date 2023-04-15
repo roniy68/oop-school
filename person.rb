b52d8f64b3b6a2bf328b462f595bd954ac5e3739
roniy68 <ahmed.11900449@lpu.in>
2023-04-15 16:11:17 +0530

    🔥 Change Made as Requested

--- person.rb
+++ person.rb
@@ -1,14 +1,4 @@
-class Nameable
-  attr_accessor :name
-
-  def initialize(name)
-    @name = name
-  end
-
-  def correct_name
-    raise NotImplementedError, 'Kindly Implement correct_name method to return the correct name'
-  end
-end
+require './namable'
 
 class Person < Nameable
   attr_accessor :name, :age
@@ -37,30 +27,6 @@ class Person < Nameable
 
   def of_age?
     @age >= 18
-  end
-end
-
-class BaseDecorator < Nameable
-  def initialize(nameable)
-    super
-
-    @nameable = nameable
-  end
-end
-
-class CapitalizeDecorator < BaseDecorator
-  def correct_name
-    @nameable.correct_name.capitalize
-  end
-end
-
-class TrimmerDecorator < BaseDecorator
-  def correct_name
-    if @nameable.correct_name.length > 10
-      @nameable.correct_name[0..9]
-    else
-      @nameable.correct_name
-    end
   end
 end
 
