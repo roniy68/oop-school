e1db5d978598077174ca104267b253f47bf89a6c
roniy68 <ahmed.11900449@lpu.in>
2023-04-15 12:56:42 +0530

    🔥 Modify classes

--- teacher.rb
+++ teacher.rb
@@ -1,17 +1,12 @@
-require './person'
+require_relative "person"
 
 class Teacher < Person
-  def initialize(
-    age:,
-    specialization:,
-    name: 'undefined',
-    parent_permission: false
-  )
-    super(name, parent_permission, age)
+  def initialize(specialization)
+    super(id, name, age, parent_permission, @specialization)
     @specialization = specialization
   end
 
-  def can_use_services
+  def can_use_services?
     true
   end
 end
